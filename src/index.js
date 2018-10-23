433e493e4b6fa240e36997a33f9cba037f153a57
MisaOgura <misa.ogura01@gmail.com>
2018-10-23 16:12:11 +0100

    Make colour

--- src/index.js
+++ src/index.js
@@ -15,10 +15,12 @@ const MIN_Y = 0;
 const MAX_Y =  210;
 const MIN_HEIGHT = 20;
 const MAX_HEIGHT = 200;
+
 render(
   <App />,
   document.getElementById('app')
 );
+
 var syn = new Drone();
 window.onload = function() {
   syn.start();
@@ -31,18 +33,32 @@ window.onload = function() {
   tracker.setEdgesDensity(0.1);
   window.tracking.track('#video', tracker, { camera: true });
   tracker.on('track', function(event) {
-    context.clearRect(0, 0, canvas.width, canvas.height);
     event.data.forEach(function(rect) {
       const intensity = (rect.height - MIN_HEIGHT) / MAX_HEIGHT;
       const normX = (rect.x - MIN_X) / MAX_X;
       const normY = (rect.y - MIN_Y) / MAX_Y;
       syn.update(normX, normY, intensity);
-      context.strokeStyle = '#a64ceb';
-      context.strokeRect(rect.x, rect.y, rect.width, rect.height);
-      context.font = '11px Helvetica';
-      context.fillStyle = "#fff";
-      context.fillText('x: ' + rect.x + 'px', rect.x + rect.width + 5, rect.y + 11);
-      context.fillText('y: ' + rect.y + 'px', rect.x + rect.width + 5, rect.y + 22);
+
+      // context.strokeStyle = '#a64ceb';
+      // context.strokeRect(rect.x, rect.y, rect.width, rect.height);
+      // context.font = '11px Helvetica';
+      // context.fillStyle = "#fff";
+      // context.fillText('x: ' + rect.x + 'px', rect.x + rect.width + 5, rect.y + 11);
+      // context.fillText('y: ' + rect.y + 'px', rect.x + rect.width + 5, rect.y + 22);
+
+      var red = Math.round(normX * 255);
+      var green = Math.round(255 - (normX * 255));
+      var blue;
+      if (normX < 0.5) {
+        blue = Math.round(255 - (normX * 255));
+      } else {
+        blue = Math.round(255 - ((1 - normX) * 255));
+      }
+      var alphha = 1 - normY
+
+      context.clearRect(0, 0, canvas.width, canvas.height);
+      context.fillStyle = `rgba(${red}, ${green}, ${blue}, ${alphha})`
+      context.fillRect(0, 0, canvas.width, canvas.height)
     });
   });
 };
